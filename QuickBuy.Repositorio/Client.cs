457178e7da72888aa391faf53085cdb9359314bb
kaykylopes <kaykylopes@hotmail.com>
2019-12-12 17:11:15 -0300

    Implementação do repositorios das classes

---
+++ QuickBuy.Repositorio/Client.cs
@@ -0,0 +1,23 @@
+﻿using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio
+{
+    public class Client
+    {
+        public Client()
+        {
+            var usuarioRepositorio = new UsuarioRepositorio();
+            var produo = new Produto();
+            var usuario = new Usuario();
+
+            UsuarioRepositorio.Adicionar();
+
+        }
+
+
+    }
+}
